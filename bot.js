45df0c08ead56b65759d29a3b0fed4fc4fd2ba5a
Roy-Desktop <56088145+roykanesmith@users.noreply.github.com>
2020-01-20 14:19:02 -0500

    Added linting and swapped to using discord.js instead of discord.io
    Discord.js is written in TypeScript and is far more object oriented and easier to work with than discord.io

--- bot.js
+++ bot.js
@@ -1,51 +1,43 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-var botFunctions = require('./source/functions')
+const Discord = require('discord.js');
+const logger = require('winston');
+const auth = require('./auth.json');
+const botFunctions = require('./source/functions');
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
+logger.add(new logger.transports.Console(), {
+    colorize: true,
 });
 logger.level = 'debug';
 
 // Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+const bot = new Discord.Client();
+
+bot.on('ready', () => {
+    logger.info('Bot Connected');
 });
 
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-
-bot.on('message', function (user, userID, channelID, message, evt) {
+bot.on('message', (message) => {
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
+    if (message.content.substring(0, 1) === '!' && !message.author.bot) { // won't respond to other bots
+        let args = message.content.substring(1).split(' ');
+        const cmd = args[0];
+
         args = args.splice(1);
         logger.debug(`cmd: '${cmd}'`);
         logger.debug(`args: [${args}]`);
 
-        switch(cmd) {
+        switch (cmd) {
             // !ping
             case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            case 'add':
-                bot.sendMessage({
-                    to: channelID,
-                    message: botFunctions.add(args, logger)
-                });
+                message.channel.send('Pong!');
             break;
-            // Just add any case commands if you want to..
-         }
-     }
+            case 'add':
+                message.channel.send(botFunctions.add(args, logger));
+            break;
+            default:
+        }
+    }
 });
+
+bot.login(auth.token);
