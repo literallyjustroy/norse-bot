1962d67d0b505b67a783240843d732033dd6b45e
Roy-Desktop <56088145+roykanesmith@users.noreply.github.com>
2020-01-20 14:28:47 -0500

    Removed redundent logging

--- source/functions.js
+++ source/functions.js
@@ -1,10 +1,5 @@
 const logger = require('winston');
 // Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console(), {
-    colorize: true,
-});
-logger.level = 'debug';
 
 module.exports = {
   add(args) {
