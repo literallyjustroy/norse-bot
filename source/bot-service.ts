a7be1a6222325f25bbc04c3d10c223729110f10e
Roy-Desktop <56088145+roykanesmith@users.noreply.github.com>
2020-05-10 08:50:14 -0400

    Code Cleanup & Chai

--- source/bot-service.ts
+++ source/bot-service.ts
@@ -1,5 +1,54 @@
-import { add, ping, randomImage } from './functions';
+import { validateArgs, Validator } from "./util/validator";
+import { Message } from "discord.js";
+import { ping, randomImage } from "./functions";
+import { logger } from "./util/log";
+import { ParsedMessage } from "./models/parsed-message";
 
 export class BotService {
 
+    async ping(message: Message): Promise<void> {
+        await message.channel.send(ping(message.createdTimestamp));
+    }
+
+    async add(args: string[], message: Message): Promise<void> {
+        let response: string;
+        try {
+            const nums = validateArgs(args, Validator.NUMBER, 2);
+            if (nums) {
+                response = String(nums[0] + nums[1]);
+            } else {
+                response = 'Must add 2 numbers (Ex: "!add 1 2")';
+            }
+        } catch (error) {
+            logger.error(error);
+            response = `Error adding ${args}`;
+        }
+        await message.channel.send(response);
+    }
+
+    async getImage(args: string[], message: Message): Promise<void> {
+        let response: string;
+        try {
+            const validArgs = validateArgs(args, Validator.ANY, 1);
+            if (validArgs) {
+                response = response = await randomImage(validArgs);
+            } else {
+                response = 'Must provide at least 1 search term (Ex: !get nku e-Sports)';
+            }
+        } catch (error) {
+            logger.error(error);
+            response = 'Error getting image';
+        }
+        await message.channel.send(response);
+    }
+
+}
+
+export function parseMessage(content: string, keywordString: string): ParsedMessage {
+    let args = content.substring(keywordString.length).split(' ');
+    args = args.filter((arg: string) => {
+        return arg.trim().length !== 0;
+    });
+
+    return { cmd: args[0], args: args.splice(1) };
 }
--- source/functions.ts
+++ source/functions.ts
@@ -1,63 +1,45 @@
 import { FlickrResponse } from "./models/flickr-response";
-import { logger }  from './log';
+import { logger }  from './util/log';
 import fetch from "node-fetch";
 
-export function ping(): string {
-    return 'pong';
+/**
+ * Calculates how long it took for a user's message to reach the bot (starting from when the user sent the message)
+ * User PC -> Discord's Servers -> Norsebot
+ * @param sentTime
+ */
+export function ping(sentTime: number): string {
+    return String(new Date().valueOf() - sentTime) + ' ms';
 }
 
 /**
- * Adds two numbers together
- * @param args
- * @returns {string}
+ * Returns a random image given a list of strings as keywords
+ * Hits the FlickrAPI and selects a random photo from the top 100 relevant results
+ * @param keywords A list of strings to be searched against
+ * @returns {Promise<string>} Returns a URL of a photo as a string, or an error message
  */
-export function add(args: string[]): string {
-    let response = 'Must add 2 numbers (Ex: "!add 1 2)"';
-        if (args.length === 2) {
-        const num1 = Number(args[0]);
-        const num2 = Number(args[1]);
-        logger.debug(`1: [${num1}], 2: [${num2}]`);
+export async function randomImage(keywords: string[]): Promise<string> {
+    let response = 'Error getting requested image';
+    const keyword = encodeURI(keywords.join(' '));
+    let numPhotos = 100;
+    const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&sort=relevance&per_page=100&format=json&nojsoncallback=1&api_key=0c748ca30b04100a36deb13f12b3c1d3&tags=${keyword}`;
 
-        if (isNaN(num1) || isNaN(num2)) { // if either is not NotANumber
-            response = 'Arguments must be numbers'; // Should really throw an error which is caught in bots.js
-        } else {
-            response = String(num1 + num2);
+    try {
+        const apiResponse = await fetch(url);
+        const flickrResponse = await apiResponse.json() as FlickrResponse;
+
+        if (flickrResponse.photos.photo.length === 0)
+            response = 'Search returned no results';
+        else {
+            numPhotos = flickrResponse.photos.photo.length; // reset incase less photos are available
+
+            const photoIndex = Math.floor(Math.random() * Math.floor(numPhotos)); // Random int less than num photos
+
+            const photo = flickrResponse.photos.photo[photoIndex];
+            response = `http://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
+            logger.debug(response);
         }
-    }
-    return response;
-}
-
-/**
- * Returns a random image from the given keyword
- * @param args
- * @returns {Promise<string>}
- */
-export async function randomImage(args: string[]): Promise<string> {
-    let response = `Must provide at least 1 search term (Ex: !get nku e-Sports)`;
-    if (args.length > 0) {
-        response = `Error getting requested image`;
-        const keyword = encodeURI(args.join(' ')); // combines arguments for multi-worded search
-        let numPhotos = 100;
-        const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=0c748ca30b04100a36deb13f12b3c1d3&tags=${keyword}&sort=relevance&per_page=100&format=json&nojsoncallback=1`;
-        logger.debug(url);
-        try {
-            const apiResponse = await fetch(url);
-            const flickrResponse = await apiResponse.json() as FlickrResponse;
-
-            if (flickrResponse.photos.photo.length === 0)
-                response = 'Search returned no results';
-            else {
-                numPhotos = flickrResponse.photos.photo.length; // reset incase less photos are available
-
-                const photoIndex = Math.floor(Math.random() * Math.floor(numPhotos)); // Random int less than num photos
-
-                const photo = flickrResponse.photos.photo[photoIndex];
-                response = `http://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
-                logger.debug(response);
-            }
-        } catch (error) {
-            logger.error(error);
-        }
+    } catch (error) {
+        logger.error(error);
     }
     return response;
 }
