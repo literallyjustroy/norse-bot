90d654c99f289cede0e6a59243f36b74332bd775
Roy-Desktop <56088145+roykanesmith@users.noreply.github.com>
2020-05-21 00:50:48 -0400

    Update database.ts

--- source/util/database.ts
+++ source/util/database.ts
@@ -4,7 +4,7 @@ import { Client, Guild, Role } from 'discord.js';
 import messages from '../util/messages.json';
 import { GuildMemory } from '../models/guild-memory';
 
-const db = monk(process.env.DB_LOGIN_URL || '');
+const db = monk(process.env.DB_LOGIN_URL || 'localhost');
 const guilds = db.get('guilds');
 export const logs = db.get('logs');
 
